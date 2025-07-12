1c7361b969ce28c2bc3698d8c942737b22d8feeb
Maruf Ahmed <mdahmedmaruf@gmail.com>
2025-07-12 12:17:58 +0600

    add radius

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,7 @@
 export default function Square({ value, onSquareClick }) {
     return (
         <button
-            className='bg-white text-2xl border border-gray-400 h-24 w-24 m-2 cursor-pointer'
+            className='bg-white text-4xl border border-gray-400 rounded-2xl h-24 w-24 m-2 cursor-pointer'
             onClick={onSquareClick}
             type='button'
         >
